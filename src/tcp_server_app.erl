c9c8819655f69c654bea381aad9e858a7319d0a5
qiushiyu <qiusy579@mingchao.com>
2018-07-16 19:23:56 +0800

    app add start/0

--- src/tcp_server_app.erl
+++ src/tcp_server_app.erl
@@ -1,6 +1,9 @@
 -module(tcp_server_app).
 -behaviour(application).
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
+
+start() ->
+    tcp_supervisor:start_link().
 
 start(normal, _StartArgs) ->
     tcp_supervisor:start_link().
